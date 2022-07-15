43d49a7233ce37ed43b26c01e4bdae1ed9c2993a
Lopez Family <lopezfamily@lopezs-air.lan>
2022-07-15 11:10:58 -0400

    removing primary keys from db in preparation for unique primary key

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,7 +12,15 @@ class Post extends Model
     // public $timestamps = false;
     protected $guarded = [];
 
+    /**
+     * renames the created_at and updated_at column names in the db
+     */
     const CREATED_AT = 'created_date';
-
     const UPDATED_AT = 'updated_date';
+    /**
+     * remove primary keys from a table in the database
+     */
+    protected $primaryKey = null;
+    public $incrementing = false;
+    
 }
